588126368ae530d014966610e6d9507a0ecc5661
Marcelo Luis <marceloluis257gh@gmail.com>
2025-11-21 21:46:31 -0300

    parser

--- src/assembler/assembler.nim
+++ src/assembler/assembler.nim
@@ -1,110 +1,7 @@
-# Este modulo é responsavel por carregar programas e processar eles para garantir
-# a sintaxe correta de cada instrução, remover coisas desnecessarias como comentarios
-# e linhas em brancos, e, fazer a troca dos labels de jumps pelo endereço.
-
-import sintaxes
-import strutils
-import strformat
-import sequtils
-import tables
-import sugar
+import tokenizer
+import parser
 import os
 
-type 
-    type_of_instruction = enum 
-        NO_ARGS, 
-        ONE_ARG, 
-        MULT_ARGS, 
-        JUMP_LABEL, 
-        ROM_LABEL
- 
-    TokenType = enum 
-        INSTRUCTION,  # instrução
-        ARGUMENT,     # argumento para instrução
-        LABEL_DEF,    # definição de label
-        LABEL_REF,    # uso de labels
-        TYPE,         # .string, .word...
-        STRING,
-        NUMBER,
-        NEW_LINE,
-        COMMA
-
-    Token* = ref object 
-        tokenType*: TokenType
-        value: string
-        line: int
-        column: int
-
-    AssemblyTokenizer* = object
-        source: string  
-        position: int   
-        line: int   
-        column: int 
-        tokens: seq[Token]  
-
-proc newAssemblyTokenzier*(file: string): AssemblyTokenizer =
-    result.source = file
-    result.position = 0
-    result.line = 1
-    result.column = 1
-    result.tokens = @[]
-
-proc advance(self: var AssemblyTokenizer) =
-    self.position += 1
-    self.column += 1
-
-proc addToken(self: var AssemblyTokenizer, token_t: TokenType, value: string) =
-    let t: Token = Token(
-        tokenType: token_t,
-        value: value,
-        line: self.line,
-        column: self.column
-    )
-
-    self.tokens.add(t)
-
-proc currentChar(self: AssemblyTokenizer): char = 
-    if self.position < self.source.len:
-        return self.source[self.position]
-    else:
-        return '\0' # Fim do arquivo
-
-# proc handleWhiteSpace(self: AssemblyTokenizer) = 
-
-
-proc tokenize*(self: var AssemblyTokenizer): seq[string] =
-    while self.position < self.source.len:
-        
-        case self.currentChar
-            of ' ', '\t', '\n', '\r':
-                self.handleWhitespace()
-            of ';':
-                self.handleComment()
-            of '"':
-                self.handleString()
-            of ':':
-                self.handleLabelDef()
-            of ',':
-                self.addToken(tkComma, ",")
-                self.advance()
-            of '.':
-                self.handleDirective()
-            of 'a'..'z', 'A'..'Z', '_':
-                self.handleIdentifier()
-            of '0'..'9', '-':
-                self.handleNumber()
-            else:
-                self.advance()  # Ignora caracteres inválidos
-    return
-
-# Removes all comments
-proc remove_comments(lines: var seq[string]) =
-    for i in 0..(lines.len() - 1) :
-        if ('#' in lines[i]) :
-            lines[i] = lines[i].split("#")[0]
-
-
-# Carrega o programa e sanetiza 
 proc load_program*(file_path: string): bool =
     if not fileExists(file_path) :
         echo "file: ", file_path, "not found"
@@ -112,8 +9,19 @@ proc load_program*(file_path: string): bool =
 
     var raw_file: string = readFile(file_path)
 
-    var at: AssemblyTokenizer = newAssemblyTokenzier(raw_file)
+    var tokenizer: Tokenizer = newTokenzier(raw_file)
+    let (tokens, ok1) = tokenizer.tokenize()
+    if (not ok1) :
+        return false
 
+    var parser: Parser = newParser(tokens)
+    let (instructions, correct) = parser.parse()
+    if not correct :
+        return false
+
+    for ins in instructions :
+        echo ins
     
+
 
     return true
--- src/test_files/file_1.asm
+++ src/test_files/file_1.asm
@@ -1,45 +1,131 @@
 .text
-    j main
+    j *main
 
     fatorial:
-        pop     $t0
         move    $rt, $t0
         li      $t1, 1      
 
 _loop_1:
-        bgt     $t0, $t1, exit    # if x > 1 then goto target
+        bgt     $t0, $t1, *exit   # if x > 1 then goto target
         mult    $rt, $rt, $t0     #   rt *= x 
         subi    $t0, $t0, 1       #   $t0 = $t0 -1
-        j       _loop_1
-_exit:
+        j       *_loop_1
+_exit:         
         return
             
 
     print:
-        pop $t0
-        li $sc 1 # Syscall para pritnar numero
+        lw $t0, 0($sp)
+    
+        li $sc, 1 # Syscall para pritnar numero
+        li $sc, 1 # Syscall para pritnar numero
         syscall
         return
 
     main:
-        li $t0 5
-        push $t0
+        li $t0, 5
         
-        jal fatorial  # Fatorial de 5
-        push $rt
+        jal *fatorial  # Fatorial de 5
 
-        jal print
+        jal *print
         
         # exit
         li $sc, 0
         syscall 
 
+_3_registradores:
+    add     $rt, $rt, $t0
+    sub     $rt, $rt, $t0
+    mult    $rt, $rt, $t0
+    div     $rt, $rt, $t0
+    or      $rt, $rt, $t0
+    and     $rt, $rt, $t0
+    slt     $rt, $rt, $t0
         
+_imediatos:
+    addi    $rt, $rt, 3000
+    subi    $rt, $rt, 3000
+    multi   $rt, $rt, 3000
+    divi    $rt, $rt, 3000
+    ori     $rt, $rt, 3000
+    andi    $rt, $rt, 3000
+    slti    $rt, $rt, 3000
+
+_branch:
+    beq $rt, $rt, *GOTO 
+    bne $rt, $rt, *GOTO 
+    bgt $rt, $rt, *GOTO 
+    bge $rt, $rt, *GOTO 
+    blt $rt, $rt, *GOTO 
+    ble $rt, $rt, *GOTO
+
+print:
+    li $sc, 1 # print
+    syscall
+    return
+
+soma_valores:
+    addi $sp, $sp, 2 # $sp = 14
+    sw $fp, 0($sp)   # salva fp na pilha
+    move $fp, $sp    # fp = sp
+
+    li $rt, 0
+
+    lw $t0, -4($sp)
+    lw $t1, -6($sp)
+    add $rt, $t0, $t1 # rt = t0 + t1
+
+    lw $t0, -8($sp)
+    add $rt, $rt, $t0
+    
+    lw $fp, 0($sp)
+
+    return
+
+game:
+    # $sp = 0
+    # $fp = 0
+
+    li $t0, 100      # player_x
+    addi $sp, $sp, 2 # $sp = 2
+    sw $t0, 0($sp)   # push player_x na stack
+    
+    li $t0, 50       # player_x 
+    addi $sp, $sp, 2 # $sp = 4 
+    sw $t0, 0($sp)   # push player_y na stack
+    
+    li $t0, 10       # player_hp - 8 bits
+    addi $sp, $sp, 2 # $sp = 6
+    sb $t0, 0($sp)   # salva player_hp na stack
+    # $sp = 6
+
+    lrw $t0,  0(*notas) # $t0 = 124
+    lrw $t1, -2(*notas) # $t1 = 125
+    lrw $t2, -4(*notas) # $t2 = 125
+
+    addi $sp, $sp, 6 # $sp = 12
+    sw $t0, -4($sp)
+    sw $t1, -2($sp)
+    sw $t2,  0($sp)
+
+    jal *soma_valores
+
+    li $sc, 1       # system call #1 - print int
+    move $t0, $rt   # t0 = rt
+    syscall         # execute
+
+
 .data
     nome:  .string "Marceline"
     hello: .string "Hello World"
     nomes: .string "Reimu Hakurei" "Marisa Kirisame" "Alice"
 
     idade: .int8   23
-    notas: .int16  123 123 123
-    
+    notas: .int16  124 125 126
+
+    # str_errada1: .string 123
+    # str_errada2: .string hello
+
+    # num_errado1: .int16 "hello"
+
+    player_sprite: .string "A"  
